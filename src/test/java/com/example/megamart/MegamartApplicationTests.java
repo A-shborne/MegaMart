8432f08765e2d7757a6946f45367fa14ea59f146
Ajith <reach@Ajith>
2023-11-15 16:28:22 +0530

    "intial setup"

---
+++ src/test/java/com/example/megamart/MegamartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.megamart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegamartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
